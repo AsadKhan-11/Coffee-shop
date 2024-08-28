b9fd77db6bb94ef538614f8b040a5af70b36e234
Asad Khan <mrasad10khan@gmail.com>
2024-08-28 20:41:38 +0500

    Creating Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Components/Home/Home";
+import Menu from "./Components/Menu/Menu";
+import coffee from "../public/Coffee1.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="overall">
       <Nav />
       <Home />
-    </>
+      <div className="home-img-container">
+        <img src={coffee} className="home-img"></img>
+      </div>
+      <div className="ribbon1 ribbon">
+        <h3>Mocha</h3>
+        <h3>Latte</h3>
+        <h3>Espresso</h3>
+      </div>
+      <div className="ribbon2 ribbon">
+        <h3>Mocha</h3>
+        <h3>Latte</h3>
+        <h3>Espresso</h3>
+      </div>
+      <Menu />
+    </div>
   );
 }
 
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import coffee from "../../../public/Coffee1.png";
+import "./Menu.css";
+function Menu() {
+  return (
+    <div className="Menu">
+      <h1 className="menu-header">OUR MENU</h1>
+    </div>
+  );
+}
+
+export default Menu;
